799c71a780ea09d6f1de5df6cd7640a205da4fc8
Fulvio Corno <fulvio.corno@polito.it>
2020-04-16 17:45:21 +0200

    Progetto base

---
+++ src/main/java/it/polito/tdp/quadrato/model/RisolviQuadrato.java
@@ -0,0 +1,59 @@
+package it.polito.tdp.quadrato.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RisolviQuadrato {
+	private int N ; // lato del quadrato
+	private int N2 ; // numero di caselle (N^2)
+	private int magica ; // costante magica
+	
+	
+	/**
+	 * Verifica se una soluzione rispetta tutte le somme
+	 * @param parziale
+	 * @return
+	 */
+	private boolean controlla(List<Integer> parziale) {
+		if(parziale.size()!=this.N*this.N)
+			throw new IllegalArgumentException("Numero di elementi insufficiente") ;
+		
+		// Fai la somma delle righe
+		for(int riga=0; riga<this.N; riga++) {
+			int somma = 0 ;
+			for(int col=0; col<this.N; col++) {
+				somma += parziale.get(riga*this.N+col) ;
+			}
+			if(somma!=this.magica)
+				return false ;
+		}
+		
+		// Fai la somma delle colonne
+		for(int col=0; col<this.N; col++) {
+			int somma = 0 ;
+			for(int riga=0; riga<this.N; riga++) {
+				somma += parziale.get(riga*this.N+col) ;
+			}
+			if(somma!=this.magica)
+				return false ;
+		}
+		
+		// diagonale principale
+		int somma = 0;
+		for(int riga=0; riga<this.N; riga++) {
+			somma += parziale.get(riga*this.N+riga) ;
+		}
+		if(somma!=this.magica)
+			return false ;
+		
+		// diagonale inversa
+		somma = 0;
+		for(int riga=0; riga<this.N; riga++) {
+			somma += parziale.get(riga*this.N+(this.N-1-riga)) ;
+		}
+		if(somma!=this.magica)
+			return false ;
+
+		return true ;
+	}
+}
